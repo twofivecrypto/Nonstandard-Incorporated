4f77903ef13088bf089e2367b73519217b7be62a
Mansa <twofivecrypto@gmail.com>
2023-06-20 10:51:44 -0400

    yes

--- mansaba_forever/src/App.js
+++ mansaba_forever/src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function WelcomeMessage() {
-  return <p>Welcome to the Eternal Mind of the Mansa</p>;
+function WelcomeMessage({ children }) {
+  return <p>{children}</p>;
 }
 
 function App() {
   const message = 'Hello';
+
+  const Counter = () => {
+
+    const [count, setCount] = useState(0);
+    const [anotherCounter, setAnotherCounter] = useState(0);
+
+    return (
+      
+      <div>
+        <div>
+        <p>You clicked me {count} times...</p>
+        <button onClick={() => setCount(count + 1)}>Click me!</button>
+      </div>
+      <div>
+        <p>and this was clicked {anotherCounter} times</p>
+        <button onClick={() => setAnotherCounter(anotherCounter + 1)}>Click me!</button>
+      </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <WelcomeMessage />
-        {message === 'Hello' ? <p>The message was "Hello!"</p> : null} {/* Render the result of the expression */}
+        const [count, setCount] = useState(0)
+        <Counter />
+        <WelcomeMessage>Here is some message</WelcomeMessage>
+        {message === 'Hello' ? <p>The message was "Hello!"</p> : null}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -27,4 +51,6 @@ function App() {
   );
 }
 
-export default App;
+
+ReactDOM.render(<App />, document.getElementById('root'));
+export default App
